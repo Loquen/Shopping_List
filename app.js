90e991f23dd40ac35f1549e5325de38fdbceabf8
Loquen Jones <lrjones416@gmail.com>
2014-07-17 13:11:16 -0700

    Added append functionality

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-	
+
+	$(".add").on("click", function() {
+		/*Select text input div, grab text from text box
+		create new HTML Radio button w/ unchecked class */
+		var input = $('#textField').val();
+		var clear = $("#textField").val("");
+		
+		$('.list').append("<li class ='unchecked item'><input type='checkbox' name='listItem' value='10'>" + input + "</li>");
+		clear;
+	});
 });
