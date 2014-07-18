427e9df5d26d508d9ee829c19008f942f81cbcea
Loquen Jones <lrjones416@gmail.com>
2014-07-18 12:19:40 -0700

    Working on Checkbox functionality

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 $(document).ready(function() {
 
+	var number = 1;
 	$(".add").on("click", function() {
 		/*Select text input div, grab text from text box
 		create new HTML Radio button w/ unchecked class */
 		var input = $('#textField').val();
 		var clear = $("#textField").val("");
 		
-		$('.list').append("<li class ='unchecked item'><input type='checkbox' name='listItem' value='10'>" + input + "</li>");
-		clear;
+		
+		if(input != "") {
+			$('#list').append("<input class ='unchecked item' type='checkbox' name='listItem' checked='checked' value='" + number + "'>" + input + "<p>" );
+			clear;
+			
+		}
+		number++;
 	});
+
+	var selected = [];
+	$('#list').each(function() {
+		if( $(this).prop("checked") ){
+			selected.push($(this).attr('name'));
+			console.log(selected);
+		}
+		
+	})
+	/*$(".list").change(function(){
+		if($("input:checkbox").checked) {
+			// Change class to "checked"
+			console.log("checked");
+			this.toggleClass("checked unchecked");
+		} else {
+			// Change class to "unchecked"
+			console.log("unchecked");
+			this.toggleClass("checked unchecked");
+		}
+	})*/
 });
